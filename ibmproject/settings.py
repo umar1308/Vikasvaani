f22405008493224d1485de0fb4e2a6a828e1ddea
Md. Umar <129945957+umar1308@users.noreply.github.com>
2024-08-01 18:51:14 +0530

    Update settings.py

--- ibmproject/settings.py
+++ ibmproject/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL='static/'
-MEDIA_URL='staticfiles/'
+MEDIA_URL='admin/'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_ROOTS=os.path.join(BASE_DIR,'staticfiles/')
+MEDIA_ROOTS=os.path.join(BASE_DIR,'admin/')
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
